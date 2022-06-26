d5d9143bfdfe919dadc3814f2b25dc41fe9179d1
3faze <93428357+3faze@users.noreply.github.com>
2022-06-26 19:51:38 +0100

    Update intrepreter.py

--- intrepreter.py
+++ intrepreter.py
@@ -1,28 +1,43 @@
 from sys import *
 
+vars = {}
+
 def open_file(filename):
     data = open(filename, "r").read()
+    data = data.replace('\n', '')
+    data = data.split(';')
+    data = data[:-1]
     return data
 
 def parse(filecontents):
-    fl = ""
-    if "print " in filecontents:
-        fl = filecontents.replace("print ", "")
-        fl = fl.replace("\n", "")
-        fl = fl.replace("\"", "")
-        if ";" in fl:
-            if "; " in fl:
-                fl = fl.replace("; ", "\n")
-            else:
-                fl = fl.replace(";", "\n")
+    for i in filecontents:
+        if i[0:5] == "print":
+            text = i.replace("print", '')
+            quotes = []
+            spaces = []
+            for j in range(len(text)):
+                if text[j] == "\"":
+                    quotes.append(j)
+                if text[j] == " ":
+                    spaces.append(j)
+        elif i[0:3] == "let":
+            let = i.replace("let", "")
+            firstquote = let.index("\"")
+            
+            let = let.replace(" ", "")
+            equals = 0
+            for j in range(len(let)):
+                if let[j] == "=":
+                    equals = j
 
-    if fl is not None:
-        return fl
-    else:
-        return ""
-        
+            lets[let[0:equals]] = let[equals+1:].replace("\"", "")
+
+            print(text[quotes[0] + 1:quotes[1]])
+
+            
 def run():
     data = open_file(argv[1])
-    print(parse(data))
+    parse(data)
 
 run()
+
